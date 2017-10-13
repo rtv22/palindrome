3de743fe08ce4a6d44b595302286af99c6c3e332
rtv22 <rtvcojigor@gmail.com>
2017-10-13 21:00:54 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -6,3 +6,18 @@ SCENARIO("init", "[init]") {
 	REQUIRE(array_size_() == 0);
 	REQUIRE(count_() == 0);
 }
+
+SCENARIO("Push", "[push]") {
+	stack<int> A;
+	A.push(1);
+	A.push(2);
+	REQUIRE(REQUIRE(count_() == 2));
+}
+
+SCENARIO("Pop", "[pop]") {
+	stack<int> A;
+	A.push(1);
+	A.push(2);
+	A.pop();
+	REQUIRE(count_() == 1));
+}
